607f9515e20709ee2beb9fcc682e3f7a38e14be2
terist999 <xscammer25@gmail.com>
2025-08-05 23:52:57 +0800

    Update LimitHub_Luarmor_E.lua

--- LimitHub_Luarmor_E.lua
+++ LimitHub_Luarmor_E.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/QP6PVofSPSnwtcwi/raw", true))()
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/FakeModz/LimitHub/refs/heads/main/LimitHub_Luarmor_E.lua')))()
+loadstring(game:HttpGet("https://pastefy.app/fthRUaRN/raw"))()
